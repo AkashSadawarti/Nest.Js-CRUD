45b060f4db73fc893325e3b9567b6e5d712299d9
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-08-18 12:39:14 +0530

    user dto updates

---
+++ nest-crud-app/src/DTO/user.dto.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { IsEmail, IsString } from "class-validator"
+
+
+export class UserDto {
+
+    @IsString()
+    name : string;
+
+    @IsString()
+    surname : string;
+
+    @IsEmail()
+    email : string;
+  
+    // @IsEmpty()
+    // token : string
+
+}
