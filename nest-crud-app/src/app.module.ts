1d8ebe53bfc466c006b516fd96a216e1350a895f
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-08-20 19:45:05 +0530

    file update

--- nest-crud-app/src/app.module.ts
+++ nest-crud-app/src/app.module.ts
@@ -6,6 +6,8 @@ import { UserModule } from './user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
+import { AuthModule } from './auth/auth.module';
+import { ProfileModule } from './profile/profile.module';
 
 
 
@@ -14,20 +16,20 @@ import { User } from './entity/user.entity';
 @Module({
   controllers: [AppController],
   imports: [ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [User],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),UserModule ],
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+      type: 'postgres',
+      host: configService.get('DB_HOST'),
+      port: +configService.get<number>('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_NAME'),
+      entities: [User],
+      synchronize: true,
+    }),
+    inject: [ConfigService],
+  }), UserModule, AuthModule, ProfileModule],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
