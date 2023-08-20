be76388661fd7175f1b1f58435003dde1a7bc0f0
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-08-20 19:44:53 +0530

    file update

--- nest-crud-app/src/app.controller.ts
+++ nest-crud-app/src/app.controller.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+
 
 @Controller() //if u give route here it would be prefix for all api as it will more reusable
 export class AppController {
@@ -11,9 +12,9 @@ export class AppController {
     return this.appService.getHello();
   }
 
-    // route has been given for api
-    @Get('/object')
-    getUserObject(): any {
-      return { name: 'Akash', surname: 'Sadawarti' };
-    }
+  // route has been given for api
+  @Get('/object')
+  getUserObject(): any {
+    return { name: 'Akash', surname: 'Sadawarti' };
+  }
 }
