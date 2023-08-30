3b730dbb2c9a538fce115846c5a78080f183bf18
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-08-30 13:40:12 +0530

    main.ts file update

--- nest-crud-app/src/main.ts
+++ nest-crud-app/src/main.ts
@@ -1,6 +1,8 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,17 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  //Swagger
+  const config = new DocumentBuilder()
+  .setTitle('CRUD-app API Docs')
+  .setDescription('The CRUD-app API description')
+  .setVersion('1.0')
+  .addTag('CRUD-app')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
