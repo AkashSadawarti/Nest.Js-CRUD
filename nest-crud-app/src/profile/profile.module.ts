6adf3f848c17c017b1ce914a9900363ede05b960
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-08-31 11:58:32 +0530

    profile update

---
+++ nest-crud-app/src/profile/profile.module.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { ProfileController } from './profile.controller';
+
+
+@Module({
+  controllers: [ProfileController]
+})
+export class ProfileModule {}
